8d57bb0e36dbb1461e2bd4441f6577a2915095dd
Omar awel <omerawel7@gmail.com>
2025-07-23 13:52:13 +0000

    Entferne für mobile Gerät anmelde button es reich konto erstellen button

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -96,7 +96,7 @@ export function AppHeader() {
           </DropdownMenu>
         ) : (
           <>
-            <Button variant="ghost" asChild>
+            <Button variant="ghost" asChild className="hidden sm:inline-flex">
                 <Link href="/login">
                     <LogIn />
                     Anmelden
