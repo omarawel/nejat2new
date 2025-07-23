762cc0d3a5ebc2bb1af3ba865d8c0d7c8221abd5
Omar awel <omerawel7@gmail.com>
2025-07-23 17:19:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -35,7 +36,17 @@ const nextConfig: NextConfig = {
       "https://6000-firebase-studio-1753240519809.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev",
       "https://9000-firebase-studio-1753240519809.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev",
     ]
-  }
+  },
+  webpack: (
+    config,
+    { isServer }
+  ) => {
+    if (!isServer) {
+        config.externals.push('async_hooks');
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
