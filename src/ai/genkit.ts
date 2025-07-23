cd2a44cac1ad6b68f629d102d3c90bbb0656235c
Omar awel <omerawel7@gmail.com>
2025-07-23 22:04:41 +0000

    enferne unbenötigten daten

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,9 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import { summarizeIslamicHistory } from "./flows/summarize-islamic-history";
 
 export const ai = genkit({
   plugins: [googleAI()],
   model: 'googleai/gemini-2.0-flash',
   flows: {
-    summarizeIslamicHistory,
   },
 });
