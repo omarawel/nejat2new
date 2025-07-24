67e355411169c6f1e0927ee10e1a0a725aee79e5
Omar awel <omerawel7@gmail.com>
2025-07-24 21:12:15 +0000

    Error: A component is changing an uncontrolled input to be controlled. T

--- src/components/admin/add-ad-form.tsx
+++ src/components/admin/add-ad-form.tsx
@@ -112,7 +112,7 @@ export function AddAdForm({ onFinished }: { onFinished: () => void }) {
         defaultValues: {
             title: "",
             description: "",
-            slotId: undefined,
+            slotId: "",
             linkUrl: "",
             actionButtonText: "",
             imageSource: "upload",
