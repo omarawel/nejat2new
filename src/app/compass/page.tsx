c057ba4a46cec5b4c2b3b8e98623547c83bc0683
Omar awel <omerawel7@gmail.com>
2025-07-24 10:23:28 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="absolu

--- src/app/compass/page.tsx
+++ src/app/compass/page.tsx
@@ -96,7 +96,7 @@ export default function CompassPage() {
         try {
             // Request camera permission
             try {
-                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
                 setHasCameraPermission(true);
                 if (videoRef.current) {
                     videoRef.current.srcObject = stream;
@@ -253,4 +253,3 @@ export default function CompassPage() {
         </div>
     );
 }
-
