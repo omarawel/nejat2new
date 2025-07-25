cd1d55916dd8a842b6aa2cf9fd28fbd7bef5c331
Omar awel <omerawel7@gmail.com>
2025-07-25 21:09:00 +0000

    ist diese konfiguration richtig für diese projekt¿ const firebaseConfig

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,14 +7,15 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "nejat-ba9qt",
-  "appId": "1:727421281697:web:55de49f0402160105897e0",
-  "storageBucket": "nejat-ba9qt.appspot.com",
-  "apiKey": "AIzaSyBjPIdjsQjUIOogs0LMp_KSuDpD2490HyM",
-  "authDomain": "nejat-ba9qt.firebaseapp.com",
-  "measurementId": "G-S3XQ0KYYJ6",
-  "messagingSenderId": "727421281697"
+  apiKey: "AIzaSyBjPIdjsQjUIOogs0LMp_KSuDpD2490HyM",
+  authDomain: "nejat-ba9qt.firebaseapp.com",
+  projectId: "nejat-ba9qt",
+  storageBucket: "nejat-ba9qt.appspot.com",
+  messagingSenderId: "727421281697",
+  appId: "1:727421281697:web:55de49f0402160105897e0",
+  measurementId: "G-S3XQ0KYYJ6"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
