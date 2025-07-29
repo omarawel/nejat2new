f815d5dd74164496f8fc00454446d89edc1d3a75
Omar awel <omerawel7@gmail.com>
2025-07-29 17:15:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,21 +1,20 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
+import { getFunctions } from "firebase/functions";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD5jMnxMBVrLlJbLHyvc_SvgbGYT_2-iPU",
-  authDomain: "nejat-50nk7.firebaseapp.com",
-  databaseURL: "https://nejat-50nk7-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "nejat-50nk7",
-  storageBucket: "nejat-50nk7.firebasestorage.app",
-  messagingSenderId: "623928651663",
-  appId: "1:623928651663:web:7566fc6754017248c925fa"
+  apiKey: "AIzaSyAmvETQfNyHc6cwokHhmagmxOsTPODoCK8",
+  authDomain: "nejat33.firebaseapp.com",
+  projectId: "nejat33",
+  storageBucket: "nejat33.firebasestorage.app",
+  messagingSenderId: "1006060080887",
+  appId: "1:1006060080887:web:e0add455d200c54f378df2"
 };
 
 
@@ -24,6 +23,7 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const functions = getFunctions(app);
 
 // Initialize Analytics if not already initialized
 let analytics;
@@ -32,4 +32,4 @@ if (typeof window !== 'undefined') {
 }
 
 
-export { app, auth, db, storage, analytics };
+export { app, auth, db, storage, functions, analytics };
