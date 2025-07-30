861056ad4f81b6f9b1fa26f547e8a26288eedaec
Omar awel <omerawel7@gmail.com>
2025-07-30 10:15:14 +0000

    It appears my attempt to fix the type error by assuming the standard Pag

--- src/app/admin/content/[slug]/page.tsx
+++ src/app/admin/content/[slug]/page.tsx
@@ -13,7 +13,7 @@ interface PageProps {
 }
 
 function toTitleCase(str: string) {
-    return str.replace(/-/g, ' ').replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+    return str.replace(/-/g, ' ').replace(/wS*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
 }
 
 export default function AdminContentPage({ params }: PageProps) {
